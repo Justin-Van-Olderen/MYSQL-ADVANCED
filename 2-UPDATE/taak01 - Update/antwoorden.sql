74d95d118076d46c2f021d1863b83a3a530d63d8
Justin van Olderen <2107471@talnet.nl>
2020-11-05 13:40:35 +0100

    2-UPDATE AF

--- 2-UPDATE/taak01 - Update/antwoorden.sql
+++ 2-UPDATE/taak01 - Update/antwoorden.sql
@@ -1,9 +1,10 @@
--- Opdracht 1
-
+-- Opdracht 1 
+UPDATE `studenten2` SET woonplaats = 'Amstelveen'
 -- Opdracht 2 
-
+UPDATE `studenten2` SET adres = 'kaperstraat 9' , postcode = '1332AP' WHERE student_id IN  (8,9,10)
 -- Opdracht 3
-
+UPDATE `studenten2` SET geboortedatum = '2000-12-12' WHERE student_id IN  (2)
 -- Opdracht 4
-
+UPDATE `studenten2` SET klas = '9B' WHERE student_id IN  (4,8)
 -- Opdracht 5
+UPDATE `studenten2` SET voornaam = 'Johannah' WHERE student_id IN  (6)
